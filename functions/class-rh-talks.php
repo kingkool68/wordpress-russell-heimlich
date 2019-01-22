2f2153770f9198c78330174a4eeeb813fbc2c08d
Russell Heimlich <kingkool68@gmail.com>
2019-01-22 12:15:25 -0500

    Talk details can now be saved and retrieved.
    Next up templating...

--- functions/class-rh-talks.php
+++ functions/class-rh-talks.php
@@ -4,7 +4,9 @@
  */
 class RH_Talks {
 
-	public $post_type = 'talk';
+	public static $post_type = 'talk';
+
+	public static $post_meta_key = 'rh-talk-details';
 
 	/**
 	 * Get an instance of this class
@@ -22,7 +24,7 @@ class RH_Talks {
 	public function setup_actions() {
 		add_action( 'init', array( $this, 'action_init' ) );
 		add_action( 'add_meta_boxes', array( $this, 'action_add_meta_boxes' ) );
-		add_action( 'save_post_' . $this->post_type, array( $this, 'action_save_post_talk' ), 10, 2 );
+		add_action( 'save_post_' . static::$post_type, array( $this, 'action_save_post_talk' ), 10, 2 );
 		add_action( 'edit_form_after_title', array( $this, 'action_edit_form_after_title' ) );
 	}
 
@@ -50,7 +52,7 @@ class RH_Talks {
 			'publicly_queryable'  => true,
 			'capability_type'     => 'post',
 		);
-		register_post_type( $this->post_type, $args );
+		register_post_type( static::$post_type, $args );
 	}
 
 	/**
@@ -59,7 +61,7 @@ class RH_Talks {
 	 * @param string $post_type The post type metaboxes are being added to
 	 */
 	public function action_add_meta_boxes( $post_type = '' ) {
-		if ( $post_type !== $this->post_type ) {
+		if ( $post_type !== static::$post_type ) {
 			return;
 		}
 		add_meta_box( 'rh-talk-details', 'Talk Details', array( $this, 'handle_talk_details_metabox' ), $post_type, 'advanced', 'low' );
@@ -84,24 +86,89 @@ class RH_Talks {
 			return $post_id;
 		}
 
-		if ( ! empty( $_REQUEST['rh-talk-details-nonce'] ) && wp_verify_nonce( $_REQUEST['rh-talk-details-nonce'], $this->post_type ) ) {
-			update_post_meta( $post_id, 'rh-talk-details', (array) $_REQUEST['rh-talk-details'] );
+		if ( ! empty( $_REQUEST['rh-talk-details-nonce'] ) && wp_verify_nonce( $_REQUEST['rh-talk-details-nonce'], static::$post_type ) ) {
+			update_post_meta( $post_id, static::$post_meta_key, (array) $_REQUEST['rh-talk-details'] );
 		}
 	}
 
 	public function action_edit_form_after_title( $post ) {
-		if ( $post->post_type !== $this->post_type ) {
+		if ( $post->post_type !== static::$post_type ) {
 			return;
 		}
-		echo '<h2 style="padding: 0;">Talk Description</h2>';
+		echo '<h2 style="padding: 24px 0 0;">Talk Description</h2>';
 	}
 
-	public function handle_talk_details_metabox() {
+	public function handle_talk_details_metabox( $post ) {
+		$data    = static::get_data( $post );
 		$context = array(
-			'val'   => '',
-			'nonce' => wp_create_nonce( $this->post_type ),
+			'slides_url'        => $data['slides-url'],
+			'slides_source_url' => $data['slides-source-url'],
+			'video_url'         => $data['video-url'],
+			'video_embed'       => $data['video-embed'],
+			'event_name'        => $data['event-name'],
+			'event_url'         => $data['event-url'],
+			'nonce'             => wp_create_nonce( static::$post_type ),
 		);
 		Sprig::out( 'admin/talk-details-meta-box.twig', $context );
+	}
+
+	public static function get_data( $key = '', $post = 0 ) {
+		$post      = get_post( $post );
+		$post_meta = get_post_meta( $post->ID, static::$post_meta_key, true );
+		$output    = array(
+			'slides-url'        => '',
+			'slides-source-url' => '',
+			'video-url'         => '',
+			'video-embed'       => '',
+			'event-name'        => '',
+			'event-url'         => '',
+		);
+		foreach ( $output as $key => $val ) {
+			if ( isset( $post_meta[ $key ] ) ) {
+				$output[ $key ] = $post_meta[ $key ];
+			}
+		}
+		return $output;
+	}
+
+	public static function render_archive_item( $args = array() ) {
+		$defaults           = array(
+			'url'          => '',
+			'title'        => '',
+			'excerpt'      => '',
+			'date'         => '',
+			'display_date' => '',
+			'machine_date' => '',
+			'event_name'   => '',
+		);
+		$context            = wp_parse_args( $args, $defaults );
+		$context['title']   = apply_filters( 'the_title', $context['title'] );
+		$context['excerpt'] = apply_filters( 'the_excerpt', $context['excerpt'] );
+
+		if ( ! empty( $context['date'] ) ) {
+			$date = strtotime( $context['date'] );
+			if ( empty( $context['machine_date'] ) ) {
+				$context['machine_date'] = date( DATE_W3C, $date );
+			}
+			if ( empty( $context['display_date'] ) ) {
+				$context['display_date'] = date( get_option( 'date_format' ), $date );
+			}
+		}
+
+		return Sprig::render( 'talk-archive-item.twig', $context );
+	}
+
+	public static function render_archive_item_from_post( $post, $args = array() ) {
+		$post = get_post( $post );
+		$data = static::get_data( $post->ID );
+		$args = array(
+			'url'        => get_permalink( $post ),
+			'title'      => get_the_title( $post ),
+			'excerpt'    => get_the_excerpt( $post ),
+			'date'       => get_the_date( '', $post ),
+			'event_name' => $data['event-name'],
+		);
+		return static::render_archive_item( $args );
 	}
 }
 
--- styleguide/talk-archive-item.php
+++ styleguide/talk-archive-item.php
@@ -1,3 +1,12 @@
 <?php
-$context = array();
+$basic_args = array(
+	'url'        => 'https://example.com',
+	'title'      => 'This is a title',
+	'excerpt'    => 'This is an excerpt to describe the talk better.',
+	'date'       => date( get_option( 'date_format' ) ),
+	'event_name' => 'Event Name',
+);
+$context       = array(
+	'basic' => RH_Talks::render_archive_item( $basic_args ),
+);
 Sprig::out( 'styleguide-talk-archive-item.twig', $context );
