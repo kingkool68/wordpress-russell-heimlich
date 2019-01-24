1d20b1cccd85ba3bdb79570ffc90dc5a0727d80c
Russell Heimlich <kingkool68@gmail.com>
2019-01-24 17:22:08 -0500

    Starting to think about comments...
    Will have to implement my own Walker_Comment class to make this a bit more flexible.
    
    See https://developer.wordpress.org/reference/classes/walker_comment/

--- single.php
+++ single.php
@@ -1,8 +1,20 @@
 <?php
 setup_postdata( get_post() );
 
+$comment_args = array(
+	'echo' => false,
+);
+
+$comments = get_comments( array(
+	'post_id' => get_the_ID(),
+	'orderby' => 'comment_date_gmt',
+	'order'   => 'ASC',
+	'status'  => 'approve',
+) );
+
 $context = array(
 	'the_title'   => get_the_title(),
 	'the_content' => apply_filters( 'the_content', get_the_content() ),
+	'comments'    => wp_list_comments( $comment_args, $comments ),
 );
 Sprig::out( 'single.twig', $context );
