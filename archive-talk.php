0a7180359a211af8087b5a07770ed582902d2254
Russell Heimlich <kingkool68@gmail.com>
2019-02-03 00:39:07 -0500

    Talks aren't going to have pagination.

--- archive-talk.php
+++ archive-talk.php
@@ -2,6 +2,5 @@
 $context = array(
 	'upcoming_talks' => RH_Talks::get_upcoming_talks(),
 	'talks'          => RH_Talks::render_archive_items_from_wp_query(),
-	'pagination'     => RH_Pagination::render_from_wp_query(),
 );
 Sprig::out( 'archive-talk.twig', $context );
