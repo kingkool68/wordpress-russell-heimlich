c63d5b8b747e44554a3116bc019981366ecea6c1
Russell Heimlich <kingkool68@gmail.com>
2019-06-08 00:38:06 -0400

    Front page progress.
    Added the latest blog posts and some dummy text for the talks sections on the homepage.

--- front-page.php
+++ front-page.php
@@ -1,5 +1,19 @@
 <?php
+$latest_blog_posts_query = new WP_Query(
+	array(
+		'posts_per_page' => 5,
+		'post_status'    => 'publish',
+	)
+);
+$latest_blog_posts       = array();
+foreach ( $latest_blog_posts_query->posts as $post ) {
+	$latest_blog_posts[] = array(
+		'title' => apply_filters( 'the_title', $post->post_title ),
+		'url'   => get_permalink( $post->ID ),
+	);
+}
 $context = array(
 	'image_directory_url' => get_template_directory_uri() . '/assets/img',
+	'latest_blog_posts' => $latest_blog_posts,
 );
 Sprig::out( 'front-page.twig', $context );
