b9d8aec6b0c1f63ad861c88630b980b634b98cd9
Russell Heimlich <kingkool68@gmail.com>
2019-06-12 00:21:33 -0400

    Show the search query in the search box if provided

--- sidebar-site-nav.php
+++ sidebar-site-nav.php
@@ -1,5 +1,7 @@
 <?php
+$escaped = false;
 $context = array(
-	'site_url' => get_site_url(),
+	'site_url'     => get_site_url(),
+	'search_query' => get_search_query( $escaped ),
 );
 Sprig::out( 'sidebar-site-nav.twig', $context );
